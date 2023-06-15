03da1a0b9ef57ddd052e40b25898d0d22703b86b
samatbelentbay <belentbais03@mail.ru>
2023-06-16 01:43:28 +0600

    first commit

---
+++ js/orders.js
@@ -0,0 +1,37 @@
+
+const Orders = [
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Larvender K32',
+        productNumber: '00438',
+        paymentStatus: 'Paid',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Macbook',
+        productNumber: '00482',
+        paymentStatus: 'Refunded',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Lenovo',
+        productNumber: '00432',
+        paymentStatus: 'Refunded',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'Keychron',
+        productNumber: '00422',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
